ffa4464d8313ce8a85655b1387a579cca1d709e0
semyonsavenkov <semyonsavenkov@gmail.com>
2022-11-12 22:48:00 +0600

    v.1

---
+++ src/HW1/Calculator/Main.java
@@ -0,0 +1,19 @@
+package HW1.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator myCalc = Calculator.instance.get();
+        int a = myCalc.plus.apply(1, 2);
+        myCalc.println.accept(a);
+        int b = myCalc.minus.apply(3, 1);
+        myCalc.println.accept(b);
+        int c = myCalc.devide.apply(6, 2);
+        myCalc.println.accept(c);
+
+        int d = myCalc.abs.apply(-7);
+        myCalc.println.accept(d);
+
+    }
+
+}
